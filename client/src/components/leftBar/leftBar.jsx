e02503700472638b79256a9617d1cebfc4849842
danghuy1101 <huynhtranthanh3333@example.com>
2025-06-21 10:39:12 +0700

    Completed Responsive Left Menu, Responsive Top Bar, Masonry Grid

--- client/src/components/leftBar/leftBar.jsx
+++ client/src/components/leftBar/leftBar.jsx
@@ -2,8 +2,29 @@ import './leftBar.css'
 
 const LeftBar = () => {
     return (
-        <div className='leftBar'>LeftBar</div>
-    )
-}
+        <div className='leftBar'>
+            <div className='menuIcons'>
+                <a href="/" className="menuIcon">
+                    <img src="/general/logo.png" alt="" className='logo' />
+                </a>
+                <a href="/" className="menuIcon">
+                    <img src="/general/home.svg" alt="" />
+                </a>
+                <a href="/" className="menuIcon">
+                    <img src="/general/create.svg" alt="" />
+                </a>
+                <a href="/" className="menuIcon">
+                    <img src="/general/updates.svg" alt="" />
+                </a>
+                <a href="/" className="menuIcon">
+                    <img src="/general/messages.svg" alt="" />
+                </a>
+            </div>
+            <a href="/" className="menuIcon">
+                <img src="/general/settings.svg" alt="" />
+            </a>
+        </div>
+    );
+};
 
 export default LeftBar
