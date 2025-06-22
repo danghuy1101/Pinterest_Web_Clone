f7dfed2d0e1a58e23c050a73902d41359fcf1921
danghuy1101 <huynhtranthanh3333@example.com>
2025-06-22 11:28:43 +0700

    sources run good

--- client/src/routes/authPage/authPage.jsx
+++ client/src/routes/authPage/authPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState } from "react";
-import Image from "../../components/image/image";
 function AuthPage() {
   const [isRegister, setIsRegister] = useState(false);
   const [error, setError] = useState("");
